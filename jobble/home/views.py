70c81f3a4b2f15b5c9ab8e1d1a9800f4addddf8d
Michael <mspinks2005@gmail.com>
2025-09-24 04:22:43 -0400

    Final Commits for the Structure. Refresh bug still occuring

--- jobble/home/views.py
+++ jobble/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -15,3 +15,4 @@ def jobs(request):
 
 def maps(request):
  return render(request, 'home/maps.html')
+
