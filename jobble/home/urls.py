84d510e283937f571f31263c6f0dc903e2a5d2f9
Michael <mspinks2005@gmail.com>
2025-09-24 01:24:14 -0400

    Created new Page Links

--- jobble/home/urls.py
+++ jobble/home/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
+    path('applications', views.applications, name='home.applications'),
+    path('jobs', views.jobs, name='home.jobs'),
+    path('maps', views.maps, name='home.maps'),
 ]
--- jobble/home/views.py
+++ jobble/home/views.py
@@ -6,3 +6,12 @@ def index(request):
     template_data['title'] = 'Jobble'
     return render(request, 'home/index.html', {
     'template_data': template_data})
+
+def applications(request):
+ return render(request, 'home/applications.html')
+
+def jobs(request):
+ return render(request, 'home/jobs.html')
+
+def maps(request):
+ return render(request, 'home/maps.html')
