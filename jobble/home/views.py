442e7608142aada484822a3ee4f7aa86809890f4
Braden Potter <braden.potter05@gmail.com>
2025-09-25 15:39:51 -0400

    Added search functionality for job postings so users can search for jobs by title.

--- jobble/home/views.py
+++ jobble/home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from jobs.models import JobPosting
 
 # Create your views here.
 def index(request):
@@ -10,8 +11,15 @@ def index(request):
 def applications(request):
  return render(request, 'home/applications.html')
 
+# Renders the jobs page while filtering results based on user input
 def jobs(request):
- return render(request, 'home/jobs.html')
+    query = request.GET.get("q")
+    jobs = JobPosting.objects.all().order_by('-date_posted')
+    
+    if query:
+        jobs = jobs.filter(title__icontains=query)
+
+    return render(request, 'home/jobs.html', {'jobs': jobs})
 
 def maps(request):
  return render(request, 'home/maps.html')
