4b13bea3e929c8f1c48fa9d4a968e8389e3753b6
BradenPotter <braden.potter05@gmail.com>
2025-10-23 15:06:24 -0400

    settings changes

--- jobble/jobble/settings.py
+++ jobble/jobble/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-04v^kv4x-54oe@xc7n3$dv=8jy0%(dz94%vruxtz-f$m$c_-4g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/bradenpotter/jobble/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
